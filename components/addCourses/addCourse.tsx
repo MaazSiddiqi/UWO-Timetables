20efc353a6a2dd4c54410d22eed42dbaef979453
Maaz Ali <maazali22@gmail.com>
2022-12-31 04:05:09 -0500

    cleaned up addCourse

--- components/addCourses/addCourse.tsx
+++ components/addCourses/addCourse.tsx
@@ -1,69 +1,30 @@
-import { useEffect, useState, useCallback } from "react"
+import { Course } from "@prisma/client"
+import { CourseData, CourseQuery } from "additional"
+import { useCallback, useEffect, useState } from "react"
 import CourseListItem from "./courseListItem"
-import QueryCourses from "./queryCourses"
 import ExpandCourse from "./expandCourse"
-import { CourseData } from "additional"
+import QueryCourses from "./queryCourses"
 
 const MAX_QUERY_SIZE = 5
-
-interface Query {
-  subjectQuery: string | null
-  levelQuery: number | null
-  termQuery: "A" | "B" | null
-  nameQuery: string | null
-}
 
 export default function AddCourses() {
   const [loaded, setLoaded] = useState(false)
 
-  const [showCourses, setShowCourses] = useState<CourseData[]>([])
-  const [selectedCourse, setSelectedCourse] = useState<CourseData | null>(null)
+  const [showCourses, setShowCourses] = useState<Course[]>([])
+  const [selectedCourse, setSelectedCourse] = useState<Course | null>(null)
 
   const [prompt, setPrompt] = useState("Popular Courses")
 
-  const query = useCallback<(args: Query) => void>(
-    ({ subjectQuery, levelQuery, termQuery, nameQuery }) => {
-      console.log("Querying courses...")
-      const checkSubject = subjectQuery !== null
-      const checkLevel = levelQuery !== null
-      const checkTerm = termQuery !== null
-      const checkName = nameQuery !== null
-
-      const result = savedCourses.filter(
-        ({ subject, level, term, name }: CourseData) =>
-          (checkSubject
-            ? subject.toUpperCase().indexOf(subjectQuery.toUpperCase()) > -1
-            : true) &&
-          (checkLevel
-            ? level.toString().indexOf(levelQuery.toString()) > -1
-            : true) &&
-          (checkTerm ? term.toUpperCase() === termQuery.toUpperCase() : true) &&
-          (checkName
-            ? name.toUpperCase().indexOf(nameQuery.toUpperCase()) > -1
-            : true),
-      )
-
-      console.log("Courses found:", result.length)
-
-      if (result.length > MAX_QUERY_SIZE) {
-        setShowCourses([])
-        setPrompt(
-          `Query too large (${result.length}), try being more specific...`,
-        )
-        return
-      }
-
-      setShowCourses(result)
-      setPrompt(`Found ${result.length} course${result.length == 1 ? "" : "s"}`)
-    },
-    [],
-  )
-
   useEffect(() => {
     const getPopularCourses = async () => {
-      const { courses } = await fetch("/api/courses/popular").then((res) =>
-        res.json(),
-      )
+      const courses = await fetch("/api/courses/popular")
+        .then((res) => res.json())
+        .then((res) => res.courses as Course[])
+        .catch((err) => {
+          console.log(err)
+          return [] as Course[]
+        })
+
       setShowCourses(courses)
       setLoaded(true)
     }
@@ -76,7 +37,16 @@ export default function AddCourses() {
       <h1 className="font-semibold text-xl">Add a class</h1>
       {!selectedCourse ? (
         <>
-          <QueryCourses runQuery={query} />
+          <QueryCourses
+            onQuery={(courses) => {
+              setShowCourses(courses)
+              setPrompt(
+                `Found ${courses.length} course${
+                  courses.length == 1 ? "" : "s"
+                }`,
+              )
+            }}
+          />
 
           <div className="flex justify-between font-xs text-slate-300">
             {/* <p>courses: {savedCourses.length}</p>
@@ -86,12 +56,11 @@ export default function AddCourses() {
           <div className="grow p-2 space-y-2 overflow-scroll">
             <p className="font-light font-mono">{prompt}</p>
             {loaded ? (
-              showCourses.length > 0 &&
-              showCourses.map((course: CourseData, idx: number) => (
+              showCourses.map((course: Course, idx) => (
                 <CourseListItem
-                  key={course["name"] + course["level"] + course["term"] + idx}
+                  key={course.id + idx}
                   course={course}
-                  select={setSelectedCourse}
+                  onSelect={setSelectedCourse}
                 />
               ))
             ) : (
