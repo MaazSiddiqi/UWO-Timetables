847a3dd43f753f2b7b9072655a342acd9aaa2e1d
Maaz Ali <maazali22@gmail.com>
2022-12-31 04:05:54 -0500

    moved querying to backend

--- pages/api/courses/index.ts
+++ pages/api/courses/index.ts
@@ -1,4 +1,9 @@
+import prisma from "@lib/prisma"
+import { Course } from "@prisma/client"
+import { CourseQuery } from "additional"
 import { NextApiRequest, NextApiResponse } from "next/types"
+
+const MAX_QUERY_SIZE = 5
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,6 +16,25 @@ export default async function handler(
       return res.status(405).send({ message: "Method not allowed" })
   }
 }
-function getCourses(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).send({ courses: [] })
+
+async function getCourses(req: NextApiRequest, res: NextApiResponse) {
+  const { subjectQuery, levelQuery, termQuery, nameQuery } =
+    req.body as CourseQuery
+
+  const courses = await prisma.course.findMany({})
+
+  const checkCourse = (course: Course) =>
+    (!!subjectQuery ? course.subjectCode === subjectQuery : true) &&
+    (!!levelQuery ? course.level === levelQuery : true) &&
+    (!!termQuery ? course.term === termQuery : true) &&
+    (!!nameQuery ? course.title === nameQuery : true)
+
+  const query: Course[] = []
+
+  while (query.length < MAX_QUERY_SIZE && courses.length > 0) {
+    const course = courses.pop() as Course
+    if (checkCourse(course)) query.push(course)
+  }
+
+  return res.status(200).send({ courses: query })
 }
