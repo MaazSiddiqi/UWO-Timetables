aed64cb5998a27a4a00085a00e35abcde47cb4d2
Maaz Ali <maazali22@gmail.com>
2022-12-31 21:29:16 -0500

    created addClass endpoint

--- pages/api/profile/[profileId]/timetables/[timetableId]/addClass.ts
+++ pages/api/profile/[profileId]/timetables/[timetableId]/addClass.ts
@@ -1,6 +1,8 @@
-import { Timetable } from "@prisma/client"
-import { NextApiRequest, NextApiResponse } from "next/types"
+import { getTimetableById } from "./../../../../../../services/timetable"
+import { TimetableValue } from "@features/activeTimetable"
 import prisma from "@lib/prisma"
+import { getSession } from "next-auth/react"
+import { NextApiRequest, NextApiResponse } from "next/types"
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,7 +14,7 @@ export default async function handler(
     default:
       return res
         .status(405)
-        .send({ message: "Method not allowed", timetables: [] })
+        .send({ message: "Method not allowed", timetable: null })
   }
 }
 
@@ -24,10 +26,26 @@ interface AddCourseRequest extends NextApiRequest {
 
 async function addCourse(
   req: AddCourseRequest,
-  res: NextApiResponse<{ message: string; error?: string }>,
+  res: NextApiResponse<{
+    message: string
+    timetable: TimetableValue | null
+    error?: string
+  }>,
 ) {
+  const session = await getSession()
   const { classId } = req.body
-  const timetableId = req.query.timetableId as string
+  const { profileId, timetableId } = req.query as {
+    timetableId: string
+    profileId: string
+  }
+
+  // if (!session) return res.status(401).send({ message: "Unauthorized" })
+
+  // if (session.profile?.id !== profileId)
+  //   return res.status(401).send({ message: "Unauthorized" })
+
+  // if (!classId) return res.status(400).send({ message: "Missing classId" })
+
   const add = await prisma.classInTimetable
     .create({
       data: {
@@ -37,10 +55,19 @@ async function addCourse(
     })
     .catch((error) => {
       console.log(error)
-      return res.status(400).send({ message: "Something went wrong", error })
+      return res
+        .status(400)
+        .send({ message: "Something went wrong", timetable: null, error })
     })
+
+  const timetable = await getTimetableById(timetableId)
+  if (!timetable)
+    return res
+      .status(404)
+      .send({ message: "Could not find timetable", timetable })
 
   return res.status(200).send({
     message: `Added class ${classId} to timetable ${timetableId}`,
+    timetable,
   })
 }
