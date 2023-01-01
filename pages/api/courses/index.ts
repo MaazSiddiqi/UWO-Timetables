9eaefd2bf817dab9bb47f9f2904fd53fb1277597
Maaz Ali <maazali22@gmail.com>
2022-12-31 21:30:15 -0500

    fixed request method bug

--- pages/api/courses/index.ts
+++ pages/api/courses/index.ts
@@ -1,16 +1,15 @@
+import { searchCourses } from "./../../../services/courses"
 import prisma from "@lib/prisma"
 import { Course } from "@prisma/client"
 import { CourseQuery } from "additional"
 import { NextApiRequest, NextApiResponse } from "next/types"
-
-const MAX_QUERY_SIZE = 5
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   switch (req.method) {
-    case "GET":
+    case "POST": //TODO change to GET
       return getCourses(req, res)
     default:
       return res.status(405).send({ message: "Method not allowed" })
@@ -21,20 +20,14 @@ async function getCourses(req: NextApiRequest, res: NextApiResponse) {
   const { subjectQuery, levelQuery, termQuery, nameQuery } =
     req.body as CourseQuery
 
-  const courses = await prisma.course.findMany({})
+  const courses = await searchCourses({
+    subjectQuery,
+    levelQuery,
+    termQuery,
+    nameQuery,
+  })
 
-  const checkCourse = (course: Course) =>
-    (!!subjectQuery ? course.subjectCode === subjectQuery : true) &&
-    (!!levelQuery ? course.level === levelQuery : true) &&
-    (!!termQuery ? course.term === termQuery : true) &&
-    (!!nameQuery ? course.title === nameQuery : true)
+  console.log({ courses })
 
-  const query: Course[] = []
-
-  while (query.length < MAX_QUERY_SIZE && courses.length > 0) {
-    const course = courses.pop() as Course
-    if (checkCourse(course)) query.push(course)
-  }
-
-  return res.status(200).send({ courses: query })
+  return res.status(200).send({ courses })
 }
