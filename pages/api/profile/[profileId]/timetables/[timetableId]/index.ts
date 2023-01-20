489ae47bcb073961813c6f3752bca7f0ab77ee1c
Maaz Ali <maazali22@gmail.com>
2023-01-20 16:32:06 -0500

    saving

--- pages/api/profile/[profileId]/timetables/[timetableId]/index.ts
+++ pages/api/profile/[profileId]/timetables/[timetableId]/index.ts
@@ -7,6 +7,8 @@ export default async function handler(
   switch (req.method) {
     case "GET":
       return getTimetable(req, res)
+    case "DELETE":
+      return deleteTimetable(req, res)
     default:
       return res
         .status(405)
@@ -29,6 +31,36 @@ const getTimetable = async (req: NextApiRequest, res: NextApiResponse) => {
   } catch (error) {
     return res
       .status(400)
+      .send({ message: "Something went wrong", timetable: null, error })
+  }
+}
+
+const deleteTimetable = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const timetableId = req.query.timetableId as string
+
+    const timetable = await prisma.timetable.delete({
+      where: { id: timetableId },
+    })
+
+    if (!timetable)
+      return res
+        .status(404)
+        .send({ message: "Timetable not found", timetable: null })
+
+    if (process.env.NODE_ENV === "development")
+      console.log(
+        `Deleted timetable ${timetableId} for profile ${timetable.profileId}`,
+      )
+
+    return res.status(200).send({
+      message: `Deleted timetable ${timetableId} for profile ${timetable.profileId}`,
+      timetable,
+    })
+  } catch (error) {
+    console.log(error)
+    return res
+      .status(500)
       .send({ message: "Something went wrong", timetable: null, error })
   }
 }
